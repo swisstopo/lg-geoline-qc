72d79d4538a1db40c42244784890297ba0aa3171
Monnerat Marc swisstopo <marc.monnerat@swisstopo.ch>
2025-03-04 14:07:03 +0100

    setting for high DPI

--- GeoLinesQC/geolines_qc_plugin.py
+++ GeoLinesQC/geolines_qc_plugin.py
@@ -18,7 +18,7 @@ from qgis.core import (
     QgsWkbTypes,
     QgsProcessingFeatureSourceDefinition,
 )
-from qgis.PyQt.QtCore import QCoreApplication, Qt, QVariant
+from qgis.PyQt.QtCore import QCoreApplication, QApplication, Qt, QVariant
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import (
     QAction,
@@ -37,6 +37,14 @@ DEFAULT_SEGMENT_LENGTH = 200.0
 
 ADD_CLIPPED_LAYER_TO_MAP = False
 DIALOG_WIDTH = 400
+
+# Enable high DPI scaling
+if hasattr(QApplication, 'setAttribute'):
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+
+# Set the environment variable for auto screen scaling
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = 1
 
 
 class ClipError(Exception):
